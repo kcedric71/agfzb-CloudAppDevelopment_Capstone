e1dfacb88558e205664a6a9e81b57f8b5a7cf9cf
kcedric71 <44753229+kcedric71@users.noreply.github.com>
2023-08-13 21:51:34 +0200

    Create main.py with ibmcloudant package

---
+++ functions/sample/python/main.py
@@ -0,0 +1,24 @@
+import sys 
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+def main(dict): 
+    authenticator = IAMAuthenticator("Your apiKey")
+    service = CloudantV1(authenticator=authenticator)
+    service.set_service_url("Your Url")
+    response = service.post_find(
+                db='reviews',
+                selector={'dealership': {'$eq': int(dict['id'])}},
+            ).get_result()
+    try: 
+        # result_by_filter=my_database.get_query_result(selector,raw_result=True) 
+        result= {
+            'headers': {'Content-Type':'application/json'}, 
+            'body': {'data':response} 
+            }        
+        return result
+    except:  
+        return { 
+            'statusCode': 404, 
+            'message': 'Something went wrong'
+            }
