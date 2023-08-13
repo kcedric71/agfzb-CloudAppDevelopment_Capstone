a3cc0415710d1f1555aa29987100fa8d5a428cf7
kcedric71 <44753229+kcedric71@users.noreply.github.com>
2023-08-13 21:52:10 +0200

    Create post_review.py

---
+++ functions/sample/python/post_review.py
@@ -0,0 +1,20 @@
+import sys
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+def main(dict):
+    authenticator = IAMAuthenticator("Your apikey")
+    service = CloudantV1(authenticator=authenticator)
+    service.set_service_url("Your Url")
+    response = service.post_document(db='reviews', document=dict["review"]).get_result()
+    try:
+    # result_by_filter=my_database.get_query_result(selector,raw_result=True)
+        result= {
+        'headers': {'Content-Type':'application/json'},
+        'body': {'data':response}
+        }
+        return result
+    except:
+        return {
+        'statusCode': 404,
+        'message': 'Something went wrong'
+        }
